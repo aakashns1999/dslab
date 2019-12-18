11dbb5df327ddcf8735bf8f50088fce923443221
aakashns1999 <59034706+aakashns1999@users.noreply.github.com>
2019-12-19 04:34:35 +0530

    Update binary_tree.c

--- binary_tree.c
+++ binary_tree.c
@@ -1,114 +1,151 @@
 #include<stdio.h>
-#include<malloc.h>
-
-
 struct node
 {
-  int info;
-  struct node *left;
-  struct node *right;
+	int info;
+	struct node *lt, *rt;
 };
-typedef struct node* NODE;
-/*getnode()*/
-NODE getnode()
-{
-  NODE temp;
-  temp=(NODE)malloc(sizeof(struct node));
-  if(temp==NULL)
-  { 
-    printf("allocation failed\n");
-    return;
-  }
-  return temp;
-}
+struct node *root=NULL;
+void bst(int);
+struct node *maketree(int);
+void setleft(struct  node*,int);
+void setright(struct  node*,int);
+void inorder(struct  node*);
+void preorder(struct  node*);
+void postorder(struct  node*);
 
-
-NODE create(NODE root,int item)
-{
-  NODE temp,cur,suc;
-  temp=getnode();
-  temp->info=item;
-  temp->left=NULL;
-  temp->right=NULL;
-  /*empty tree*/
-  if(root==NULL)
-  {
-    root=temp;
-    return root;
-  }
-  cur=root;
-  suc=root;
-  while(suc!=NULL)
-  {
-    cur=suc;
-    if(item<cur->info)
-      suc=suc->left;
-    else
-      suc=suc->right;
-  }
-  if(item<cur->info)
-    cur->left=temp;
-  else
-    cur->right=temp;
-  return root;
-}
-
-
-void inorder(NODE root)
-{
-  if(root!=NULL)
-  {
-    inorder(root->left);
-    printf("%d\t",root->info);
-    inorder(root->right);
-  }
-}
-/*preorder traversal*/
-void preorder(NODE root)
-{
-  if(root!=NULL)
-  { 
-    printf("%d\t",root->info);
-    preorder(root->left);
-    preorder(root->right);
-  }
-}
-
-
-void postorder(NODE root)
-{
-  if(root!=NULL)
-  {
-    postorder(root->right);
-    postorder(root->left);
-    printf("%d\t",root->info);
-  }
-}
 void main()
 {
-  NODE root=NULL;
-  int choice,item;
-  printf("1.CREATE \t 2.INORDER\t3.PREORDER\t4.POSTORDER\n");
-  for(;;)
-  {
-    printf("\nenter choice\n");
-    scanf("%d",&choice);
-    switch(choice)
-    {
-      case 3: printf("preorder traversal\n");
-              preorder(root);
-              break;
-      case 2: printf("inorder traversal\n");
-              inorder(root);
-              break;
-      case 4: printf("postorder traversal\n");
-              postorder(root);
-              break;
-      case 1: printf("enter item\n");
-              scanf("%d",&item);
-              root=create(root,item);
-              break;
-      default: return;
-    }
-  }
-}     
+	int ch,x;
+	while(1)
+	{
+	     printf("\n1:CREATE-TREE\n 2:INORDER\n 3:PREORDER\n 4:POSTORDER\n  5:EXIT\n");
+		printf("Enter your choice\n\n");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+				printf("\nEnter the element to be inserted\n\n");
+				scanf("%d",&x);
+				bst(x);
+				break;
+
+			case 2:
+				if(root==NULL)
+				printf("\nTree doesn't exist\n\n");
+				else
+				{
+					printf("\nInorder Traversal is\n\n");
+					inorder(root);
+				}
+				break;
+			case 3:
+				if(root==NULL)
+				printf("\nTree doesn't exist\n\n");
+				else
+				preorder(root);
+				break;
+
+			case 4:
+				if(root==NULL)
+				printf("\nTree doesn't exist\n\n");
+				else
+				postorder(root);
+				break;
+
+			case 5:
+				exit(1);
+
+			default:
+				printf("\nInvalid choice\n\n");
+
+		}
+	}
+}
+
+void bst(int x)
+{
+	struct node *p, *q;
+	if(root==NULL)
+	root=maketree(x);
+	else
+	{
+		p=q=root;
+		while(q!=NULL)
+		{
+			p=q;
+			if(p->info>x)
+			q=p->lt;
+			else
+			q=p->rt;
+		}
+		if(p->info<=x)
+		setright(p,x);
+		else
+		setleft(p,x);
+	}
+}
+
+struct  node* maketree(int x)
+{
+	struct node *p;
+	p=( struct  node*)malloc(sizeof(struct node));
+	if(p==NULL)
+	{
+		printf("\nMemory can't be allocated\n\n");
+		return 0;
+	}
+	p->info=x;
+	p->lt=p->rt=NULL;
+	return p;
+}
+
+void setleft(struct  node *p,int x)
+{
+	if(p==NULL)
+	printf("\nInsertion is not possible\n\n");
+	else
+	if(p->lt!=NULL)
+	printf("\nLeft node already exists\n\n");
+	else
+	p->lt=maketree(x);
+}
+
+void setright(struct  node *p,int x)
+{
+	if(p==NULL)
+	printf("\nInsertion is not possible\n\n");
+	else
+	if(p->rt!=NULL)
+	printf("\nRight node already exists\n\n");
+	else
+	p->rt=maketree(x);
+}
+
+void inorder(struct  node* p)
+{
+	if(p!=NULL)
+	{
+		inorder(p->lt);
+		printf("%d ",p->info);
+		inorder(p->rt);
+	}
+}
+
+void preorder(struct  node *p)
+{
+	if(p!=NULL)
+	{
+		printf("%d ",p->info);
+		preorder(p->lt);
+		preorder(p->rt);
+	}
+}
+void postorder(struct  node *p)
+{
+	if(p!=NULL)
+	{
+		postorder(p->lt);
+		postorder(p->rt);
+		printf("%d ",p->info);
+	}
+}
